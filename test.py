cb26dab2c15e30cb34680daaae6599af5d209178
aoladosu <46581811+aoladosu@users.noreply.github.com>
2020-01-07 08:43:31 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 
-people = requests.get("https://google.com")
-print(people)
+content = requests.get("https://raw.githubusercontent.com/aoladosu/Lab1/master/test.py?token=ALDMQMYLOY3BUT42FRHFWN26DXQLK")
+print(content.content)
